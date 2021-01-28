8efe91dc44d8163e92d6744df801310e7f9173c0
calcey <calcey@calceys-MacBook-Pro.local>
2021-01-28 21:00:42 +0530

    Added splash screen to ios

--- App.tsx
+++ App.tsx
@@ -8,12 +8,17 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppRoutes from './src/navigation';
 import { GlobalContextProvider } from './src/store/GlobalContext';
 import ThemeProvider from './src/theme/ThemeProvider';
 
 function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <ThemeProvider>
